bad1191434c5148edb8e9eac098f6ff285c42304
nam-spec <namrata.mohane23@spit.ac.in>
2026-02-09 14:27:08 +0530

    Implement user model and JWT authentication (register/login)  Add JWT-based user authentication with USer model

--- smart-city-complaint-system/backend/src/app.js
+++ smart-city-complaint-system/backend/src/app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+
+
 app.get("/api/health",(req,res)=>{
     res.status(200).json({status:"Backend is running"});
 });
+
 
 module.exports = app;
